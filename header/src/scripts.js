8283ac29a475450cd9ccd49a9ff9103d67e7b2b7
selim13 <selim013@gmail.com>
2021-06-15 08:29:14 +0300

    add mobile search bar

--- header/src/scripts.js
+++ header/src/scripts.js
@@ -1,1 +1,16 @@
-console.log("YOLO");
+const searchSubmit = document.querySelector('.header__search-submit');
+const searchForm = document.querySelector('.header__search');
+const searchInput = document.querySelector('.header__search-input');
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const style = window.getComputedStyle(searchInput);
+  console.log(style);
+  if (style.display === 'none') {
+    console.log('focus');
+    searchInput.focus();
+  } else {
+    console.log('submit');
+    // searchInput.submit();
+  }
+});
