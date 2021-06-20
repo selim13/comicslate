13efef1d3edd25e3e3a0516bbcdeb0a4dee07ae4
selim13 <selim013@gmail.com>
2021-06-20 14:45:37 +0300

    colors

--- header/src/scripts.js
+++ header/src/scripts.js
@@ -1,3 +1,24 @@
+/**
+ * Returns a random integer between two values, inclusive.
+ *
+ * @param {Number} min Minimum value, inclusive
+ * @param {Number} max Maximum value, inclusive
+ * @returns {Number}
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const header = document.querySelector('.header');
+if (header) {
+  header.addEventListener('mouseover', () => {
+    const rnd = getRandomInt(1, 6);
+    header.setAttribute('data-color', rnd);
+  });
+}
+
 const searchSubmit = document.querySelector('.header__search-submit');
 const searchForm = document.querySelector('.header__search');
 const searchInput = document.querySelector('.header__search-input');
